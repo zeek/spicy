a48c25296e2dd3c6a7c33a3bbe92352f0c078a3c
Benjamin Bannier <benjamin.bannier@corelight.com>
2020-05-27 15:21:10 +0200

    Move `rt::Byte` stringification tests to `to_string` test suite.

--- hilti/src/rt/tests/bytes.cc
+++ hilti/src/rt/tests/bytes.cc
@@ -18,11 +18,6 @@ TEST_CASE("iteration") {
         (void)x;
         static_assert(std::is_same_v<decltype(x), Bytes::Iterator::reference>);
     }
-
-    SUBCASE("to_string") {
-        CHECK_EQ(to_string("ABC"_b), "b\"ABC\"");
-        CHECK_EQ(to_string("\0\2\3\0\6\7A\01"_b), "b\"\\x00\\x02\\x03\\x00\\x06\\x07A\\x01\"");
-    }
 }
 
 TEST_SUITE_END();
