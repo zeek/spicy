5b01c32b9851cc717c7c3eeab8cb0b315ee70217
Benjamin Bannier <benjamin.bannier@corelight.com>
2020-06-05 11:42:58 +0200

    Prefer dedicated doctest check macros over custom boolean expressions.
    This not only makes it clearer what is being tested, but also leads to
    better error messages on test failure as macros like e.g., `CHECK_EQ`
    would print the operands.

--- hilti/src/rt/tests/stream.cc
+++ hilti/src/rt/tests/stream.cc
@@ -18,109 +18,110 @@ TEST_SUITE_BEGIN("Stream");
 
 TEST_CASE("Constructors") {
     auto b = "xyz"_b;
-    CHECK(b.size());
+    CHECK_GT(b.size(), 0);
+    CHECK_FALSE(b.isEmpty());
     auto x = Stream("xyz"_b);
-    CHECK(to_string(x) == R"(b"xyz")");
-    CHECK(! x.isEmpty());
-    CHECK(x.size().Ref() == 3);
-    CHECK(x.numberChunks() == 1);
+    CHECK_EQ(to_string(x), R"(b"xyz")");
+    CHECK_FALSE(x.isEmpty());
+    CHECK_EQ(x.size().Ref(), 3);
+    CHECK_EQ(x.numberChunks(), 1);
 
     auto y = Stream("123456789012345678901234567890123"_b); // Exceeds small buffer size.
-    CHECK(! y.isEmpty());
-    CHECK(y.size().Ref() == 33);
-    CHECK(y.numberChunks() == 1);
-    CHECK(to_string(y) == R"(b"123456789012345678901234567890123")");
+    CHECK_FALSE(y.isEmpty());
+    CHECK_EQ(y.size().Ref(), 33);
+    CHECK_EQ(y.numberChunks(), 1);
+    CHECK_EQ(to_string(y), R"(b"123456789012345678901234567890123")");
 
     auto z = x;
     x = Stream(""_b);
-    CHECK(to_string(z) == R"(b"xyz")");
-    CHECK(to_string(x) == R"(b"")");
-    CHECK(! z.isEmpty());
-    CHECK(z.size().Ref() == 3);
+    CHECK_EQ(to_string(z), R"(b"xyz")");
+    CHECK_EQ(to_string(x), R"(b"")");
+    CHECK_FALSE(z.isEmpty());
+    CHECK_EQ(z.size().Ref(), 3);
 
     z = y;
     y = Stream(""_b);
-    CHECK(to_string(z) == R"(b"123456789012345678901234567890123")");
-    CHECK(to_string(y) == R"(b"")");
-    CHECK(! z.isEmpty());
-    CHECK(z.size().Ref() == 33);
+    CHECK_EQ(to_string(z), R"(b"123456789012345678901234567890123")");
+    CHECK_EQ(to_string(y), R"(b"")");
+    CHECK_FALSE(z.isEmpty());
+    CHECK_EQ(z.size().Ref(), 33);
 
     x = Stream("xyz"_b);
     z = std::move(x);
-    CHECK(to_string(z) == R"(b"xyz")");
-    CHECK(! z.isEmpty());
-    CHECK(z.size().Ref() == 3);
+    CHECK_EQ(to_string(z), R"(b"xyz")");
+    CHECK_FALSE(z.isEmpty());
+    CHECK_EQ(z.size().Ref(), 3);
 
     y = Stream("123456789012345678901234567890123"_b); // Exceeds small buffer size.
     z = std::move(y);
-    CHECK(to_string(z) == R"(b"123456789012345678901234567890123")");
-    CHECK(! z.isEmpty());
-    CHECK(z.size().Ref() == 33);
+    CHECK_EQ(to_string(z), R"(b"123456789012345678901234567890123")");
+    CHECK_FALSE(z.isEmpty());
+    CHECK_EQ(z.size().Ref(), 33);
 
     Stream m;
-    CHECK(to_string(m) == R"(b"")");
+    CHECK_EQ(to_string(m), R"(b"")");
     CHECK(m.isEmpty());
-    CHECK(m.size().Ref() == 0);
+    CHECK_EQ(m.size().Ref(), 0);
 
     m = Stream(""_b);
-    CHECK(to_string(m) == R"(b"")");
+    CHECK_EQ(to_string(m), R"(b"")");
     CHECK(m.isEmpty());
-    CHECK(m.size().Ref() == 0);
+    CHECK_EQ(m.size().Ref(), 0);
 
     x = Stream("foo"_b);
-    CHECK(! x.isFrozen());
+    CHECK_FALSE(x.isFrozen());
     x.freeze();
     CHECK(x.isFrozen());
 
-    CHECK(Stream("abc"_b) == Stream("abc"_b));
-    CHECK(Stream("abc"_b) != Stream("def"_b));
-    CHECK(Stream("abc"_b) != Stream(""_b));
+    CHECK_EQ(Stream("abc"_b), Stream("abc"_b));
+    CHECK_NE(Stream("abc"_b), Stream("def"_b));
+    CHECK_NE(Stream("abc"_b), Stream(""_b));
 }
 
 TEST_CASE("Growing") {
     // rvalue append
     auto x = Stream("1234567890"_b);
-    CHECK(x.size().Ref() == 10);
-    CHECK(x.numberChunks() == 1);
+    CHECK_EQ(x.size().Ref(), 10);
+    CHECK_EQ(x.numberChunks(), 1);
 
     x.append(""_b);
-    CHECK(to_string(x) == R"(b"1234567890")");
-    CHECK(x.size().Ref() == 10);
-    CHECK(x.numberChunks() == 1);
+    CHECK_EQ(to_string(x), R"(b"1234567890")");
+    CHECK_EQ(x.size().Ref(), 10);
+    CHECK_EQ(x.numberChunks(), 1);
 
     x.append("1*3*5*7*9*"_b);
-    CHECK(to_string(x) == R"(b"12345678901*3*5*7*9*")");
-    CHECK(x.size().Ref() == 20);
-    CHECK(x.numberChunks() == 2);
+    CHECK_EQ(to_string(x), R"(b"12345678901*3*5*7*9*")");
+    CHECK_EQ(x.size().Ref(), 20);
+    CHECK_EQ(x.numberChunks(), 2);
 
     x.append("123456789012345"_b);
-    CHECK(to_string(x) == R"(b"12345678901*3*5*7*9*123456789012345")");
-    CHECK(x.size().Ref() == 35);
-    CHECK(x.numberChunks() == 3);
+    CHECK_EQ(to_string(x), R"(b"12345678901*3*5*7*9*123456789012345")");
+    CHECK_EQ(x.size().Ref(), 35);
+    CHECK_EQ(x.numberChunks(), 3);
 
     // lvalue append
     x = Stream("1234567890"_b);
-    CHECK(x.size().Ref() == 10);
-    CHECK(x.numberChunks() == 1);
+    CHECK_EQ(x.size().Ref(), 10);
+    CHECK_EQ(x.numberChunks(), 1);
 
     auto y1 = ""_b;
     auto y2 = "1*3*5*7*9*"_b;
     auto y3 = "123456789012345"_b;
 
     x.append(y1);
-    CHECK(to_string(x) == R"(b"1234567890")");
-    CHECK(x.size().Ref() == 10);
-    CHECK(x.numberChunks() == 1);
+    CHECK_EQ(to_string(x), R"(b"1234567890")");
+    CHECK_EQ(x.size().Ref(), 10);
+    CHECK_EQ(x.numberChunks(), 1);
 
     x.append(y2);
-    CHECK(to_string(x) == R"(b"12345678901*3*5*7*9*")");
-    CHECK(x.size().Ref() == 20);
-    CHECK(x.numberChunks() == 2);
+    CHECK_EQ(to_string(x), R"(b"12345678901*3*5*7*9*")");
+    CHECK_EQ(x.size().Ref(), 20);
+    CHECK_EQ(x.numberChunks(), 2);
 
     x.append(y3);
-    CHECK(to_string(x) == R"(b"12345678901*3*5*7*9*123456789012345")");
-    CHECK(x.size().Ref() == 35);
-    CHECK(x.numberChunks() == 3);
+    CHECK_EQ(to_string(x), R"(b"12345678901*3*5*7*9*123456789012345")");
+    CHECK_EQ(x.size().Ref(), 35);
+    CHECK_EQ(x.numberChunks(), 3);
 }
 
 TEST_CASE("Iterators") {
@@ -130,7 +131,7 @@ TEST_CASE("Iterators") {
     for ( auto i : x )
         s += i;
 
-    CHECK(s == "12345");
+    CHECK_EQ(s, "12345");
 
     x = Stream("12345"_b);
     x.append("1234567890"_b);
@@ -142,41 +143,41 @@ TEST_CASE("Iterators") {
     for ( auto i : x )
         s += i;
 
-    CHECK(s == "123451234567890123456789012345678901234567890");
+    CHECK_EQ(s, "123451234567890123456789012345678901234567890");
 
     auto i = x.safeBegin();
     i += 7;
-    CHECK(*i == '3');
+    CHECK_EQ(*i, '3');
     i += 7;
-    CHECK(*i == '0');
+    CHECK_EQ(*i, '0');
     i += 1;
-    CHECK(*i == '1');
+    CHECK_EQ(*i, '1');
 
     auto j = x.safeEnd();
-    CHECK(j != i);
-    CHECK(j == x.safeEnd());
+    CHECK_NE(j, i);
+    CHECK_EQ(j, x.safeEnd());
 
     x.append("abc"_b);
-    CHECK(j != x.safeEnd());
-    CHECK(*j == 'a');
+    CHECK_NE(j, x.safeEnd());
+    CHECK_EQ(*j, 'a');
 
     ++j;
-    CHECK(j != x.safeEnd());
+    CHECK_NE(j, x.safeEnd());
     ++j;
-    CHECK(j != x.safeEnd());
+    CHECK_NE(j, x.safeEnd());
     ++j;
-    CHECK(j == x.safeEnd());
+    CHECK_EQ(j, x.safeEnd());
 
     j += 5;
     CHECK_THROWS_AS(*j, InvalidIterator);
     x.append("1234567890"_b);
-    CHECK(*j == '6');
+    CHECK_EQ(*j, '6');
 
     x = Stream(""_b);
     i = x.safeBegin();
     CHECK_THROWS_AS(*i, InvalidIterator);
     x.append("1"_b);
-    CHECK(*i == '1');
+    CHECK_EQ(*i, '1');
 
     CHECK_THROWS_AS((void)(*j == '6'), InvalidIterator); // j now invalid.
 }
@@ -192,16 +193,16 @@ TEST_CASE("sub") {
     auto i = (x.safeBegin() + 5);
     auto j = (x.safeBegin() + 15);
 
-    CHECK(x.view().sub(i, j) == "6789012345"_b);
+    CHECK_EQ(x.view().sub(i, j), "6789012345"_b);
 
     auto y = Stream("12345"_b);
-    CHECK(y.view().sub(y.safeBegin(), y.safeEnd()) == "12345"_b);
-    CHECK(y.view().sub(y.safeBegin(), y.safeBegin()) == ""_b);
-    CHECK(y.view().sub(y.safeEnd(), y.safeEnd()) == ""_b);
+    CHECK_EQ(y.view().sub(y.safeBegin(), y.safeEnd()), "12345"_b);
+    CHECK_EQ(y.view().sub(y.safeBegin(), y.safeBegin()), ""_b);
+    CHECK_EQ(y.view().sub(y.safeEnd(), y.safeEnd()), ""_b);
 
     auto f = [](const stream::View& v) { return v.sub(v.safeBegin() + 15, v.safeBegin() + 25); };
 
-    CHECK(Bytes(f(x.view()).data()) == "6789012345"_b);
+    CHECK_EQ(Bytes(f(x.view()).data()), "6789012345"_b);
 }
 
 TEST_CASE("freezing") {
@@ -213,11 +214,11 @@ TEST_CASE("freezing") {
     x.append("E234567890"_b);
 
     auto i = (x.safeBegin() + 25);
-    CHECK(! i.isFrozen());
+    CHECK_FALSE(i.isFrozen());
     x.freeze();
     CHECK(i.isFrozen());
     x.unfreeze();
-    CHECK(! i.isFrozen());
+    CHECK_FALSE(i.isFrozen());
 }
 
 TEST_CASE("convert view to stream") {
@@ -230,9 +231,9 @@ TEST_CASE("convert view to stream") {
     x.append("ABCDEF"_b);
     x.append("GHJI"_b);
     v = stream::View(x.safeBegin() + 1, x.safeBegin() + 12);
-    CHECK(v == "2345ABCDEFG"_b);
+    CHECK_EQ(v, "2345ABCDEFG"_b);
     y = Stream(v);
-    CHECK(y == "2345ABCDEFG"_b);
+    CHECK_EQ(y, "2345ABCDEFG"_b);
 }
 
 TEST_CASE("Expanding vs non-expanding views") {
@@ -245,8 +246,8 @@ TEST_CASE("Expanding vs non-expanding views") {
     x.append("123456789D"_b);
     x.append("E234567890"_b);
 
-    CHECK(v1.size().Ref() == 55);
-    CHECK(v2.size().Ref() == 5);
+    CHECK_EQ(v1.size().Ref(), 55);
+    CHECK_EQ(v2.size().Ref(), 5);
 }
 
 TEST_CASE("Trim") {
@@ -258,42 +259,42 @@ TEST_CASE("Trim") {
 
     auto y = x;
 
-    CHECK(x.size().Ref() == 72);
-    CHECK(x.numberChunks() == 5);
+    CHECK_EQ(x.size().Ref(), 72);
+    CHECK_EQ(x.numberChunks(), 5);
 
     x.trim(x.at(10));
-    CHECK(x.size().Ref() == 62);
+    CHECK_EQ(x.size().Ref(), 62);
     x.trim(x.at(20));
-    CHECK(x.safeBegin().offset().Ref() == 20);
-    CHECK(x.size().Ref() == 52);
+    CHECK_EQ(x.safeBegin().offset().Ref(), 20);
+    CHECK_EQ(x.size().Ref(), 52);
     x.trim(x.at(32));
-    CHECK(x.size().Ref() == 40);
-    CHECK(x.numberChunks() == 4);
+    CHECK_EQ(x.size().Ref(), 40);
+    CHECK_EQ(x.numberChunks(), 4);
     x.trim(x.at(50));
-    CHECK(x.size().Ref() == 22);
-    CHECK(x.numberChunks() == 3);
+    CHECK_EQ(x.size().Ref(), 22);
+    CHECK_EQ(x.numberChunks(), 3);
     x.trim(x.at(65));
-    CHECK(x.safeBegin().offset().Ref() == 65);
-    CHECK(x.size().Ref() == 7);
-    CHECK(x == "4567890"_b);
-    CHECK(x.numberChunks() == 1);
+    CHECK_EQ(x.safeBegin().offset().Ref(), 65);
+    CHECK_EQ(x.size().Ref(), 7);
+    CHECK_EQ(x, "4567890"_b);
+    CHECK_EQ(x.numberChunks(), 1);
     x.trim(x.at(72));
-    CHECK(x.size().Ref() == 0);
-    CHECK(x == ""_b);
-    CHECK(x.numberChunks() == 1); // will stay the same
-    CHECK(x.safeBegin().offset().Ref() == 72);
+    CHECK_EQ(x.size().Ref(), 0);
+    CHECK_EQ(x, ""_b);
+    CHECK_EQ(x.numberChunks(), 1); // will stay the same
+    CHECK_EQ(x.safeBegin().offset().Ref(), 72);
 
     y.trim(y.at(100));
-    CHECK(y.size().Ref() == 0);
-    CHECK(y.safeBegin().offset().Ref() == 100);
+    CHECK_EQ(y.size().Ref(), 0);
+    CHECK_EQ(y.safeBegin().offset().Ref(), 100);
 
     auto z = Stream("12345"_b);
     z.trim(z.at(3));
-    CHECK(z == "45"_b);
-    CHECK(z.size().Ref() == 2);
+    CHECK_EQ(z, "45"_b);
+    CHECK_EQ(z.size().Ref(), 2);
     z.trim(z.at(5));
-    CHECK(z == ""_b);
-    CHECK(z.size().Ref() == 0);
+    CHECK_EQ(z, ""_b);
+    CHECK_EQ(z.size().Ref(), 0);
 }
 
 TEST_CASE("Trim with existing iterator and append") {
@@ -306,7 +307,7 @@ TEST_CASE("Trim with existing iterator and append") {
     j += 2;
     x.trim(j);
 
-    CHECK(*i == '3');
+    CHECK_EQ(*i, '3');
 }
 
 TEST_CASE("Block iteration") {
@@ -318,11 +319,11 @@ TEST_CASE("Block iteration") {
     auto block = v.firstBlock();
     CHECK(block);
     CHECK(content(block, "01234"));
-    CHECK(block->offset == 0);
-    CHECK(block->size == 5);
+    CHECK_EQ(block->offset, 0);
+    CHECK_EQ(block->size, 5);
     CHECK(block->is_first);
     CHECK(block->is_last);
-    CHECK(! v.nextBlock(block));
+    CHECK_FALSE(v.nextBlock(block));
 
     x.append("567"_b);
     x.append("890"_b);
@@ -333,63 +334,63 @@ TEST_CASE("Block iteration") {
     block = v.firstBlock();
     CHECK(block);
     CHECK(content(block, "01234"));
-    CHECK(block->offset == 0);
-    CHECK(block->size == 5);
+    CHECK_EQ(block->offset, 0);
+    CHECK_EQ(block->size, 5);
     CHECK(block->is_first);
-    CHECK(! block->is_last);
+    CHECK_FALSE(block->is_last);
     block = v.nextBlock(block);
     CHECK(block);
     CHECK(content(block, "567"));
-    CHECK(block->offset == 5);
-    CHECK(block->size == 3);
-    CHECK(! block->is_first);
-    CHECK(! block->is_last);
+    CHECK_EQ(block->offset, 5);
+    CHECK_EQ(block->size, 3);
+    CHECK_FALSE(block->is_first);
+    CHECK_FALSE(block->is_last);
     block = v.nextBlock(block);
     CHECK(block);
     CHECK(content(block, "890"));
-    CHECK(block->offset == 8);
-    CHECK(block->size == 3);
-    CHECK(! block->is_first);
-    CHECK(! block->is_last);
+    CHECK_EQ(block->offset, 8);
+    CHECK_EQ(block->size, 3);
+    CHECK_FALSE(block->is_first);
+    CHECK_FALSE(block->is_last);
     block = v.nextBlock(block);
     CHECK(block);
     CHECK(content(block, "abc"));
-    CHECK(block->offset == 11);
-    CHECK(block->size == 3);
-    CHECK(! block->is_first);
-    CHECK(! block->is_last);
+    CHECK_EQ(block->offset, 11);
+    CHECK_EQ(block->size, 3);
+    CHECK_FALSE(block->is_first);
+    CHECK_FALSE(block->is_last);
     block = v.nextBlock(block);
     CHECK(block);
     CHECK(content(block, "def"));
-    CHECK(block->offset == 14);
-    CHECK(block->size == 3);
-    CHECK(! block->is_first);
+    CHECK_EQ(block->offset, 14);
+    CHECK_EQ(block->size, 3);
+    CHECK_FALSE(block->is_first);
     CHECK(block->is_last);
-    CHECK(! v.nextBlock(block));
+    CHECK_FALSE(v.nextBlock(block));
 
     v = v.sub(v.at(6), v.at(13));
     block = v.firstBlock();
     CHECK(block);
     CHECK(content(block, "67"));
-    CHECK(block->offset == 6);
-    CHECK(block->size == 2);
+    CHECK_EQ(block->offset, 6);
+    CHECK_EQ(block->size, 2);
     CHECK(block->is_first);
-    CHECK(! block->is_last);
+    CHECK_FALSE(block->is_last);
     block = v.nextBlock(block);
     CHECK(block);
     CHECK(content(block, "890"));
-    CHECK(block->offset == 8);
-    CHECK(block->size == 3);
-    CHECK(! block->is_first);
-    CHECK(! block->is_last);
+    CHECK_EQ(block->offset, 8);
+    CHECK_EQ(block->size, 3);
+    CHECK_FALSE(block->is_first);
+    CHECK_FALSE(block->is_last);
     block = v.nextBlock(block);
     CHECK(block);
     CHECK(content(block, "ab"));
-    CHECK(block->offset == 11);
-    CHECK(block->size == 2);
-    CHECK(! block->is_first);
+    CHECK_EQ(block->offset, 11);
+    CHECK_EQ(block->size, 2);
+    CHECK_FALSE(block->is_first);
     CHECK(block->is_last);
-    CHECK(! v.nextBlock(block));
+    CHECK_FALSE(v.nextBlock(block));
 }
 
 TEST_CASE("to_string") {
