3c2d70bd62e7b22a8e2d3d85107510d00add5543
Benjamin Bannier <benjamin.bannier@corelight.com>
2020-07-08 15:29:08 +0200

    Add tests of rt::Result::valueOrThrow`.

--- hilti/src/rt/tests/result.cc
+++ hilti/src/rt/tests/result.cc
@@ -40,6 +40,31 @@ TEST_CASE("not equal") {
     CHECK_NE(Result(42), Result<int>(result::Error("foo")));
 }
 
+TEST_CASE("valueOrThrow") {
+    SUBCASE("const") {
+        const auto r1 = Result<int>(0);
+        const auto r2 = Result<int>();
+        const auto r3 = Result<int>(result::Error("foo"));
+
+        CHECK_EQ(r1.valueOrThrow(), 0);
+        CHECK_THROWS_WITH_AS(r2.valueOrThrow(), "<result not initialized>", const result::NoResult&);
+        CHECK_THROWS_WITH_AS(r3.valueOrThrow(), "foo", const result::NoResult&);
+    }
+
+    SUBCASE("non const") {
+        auto r1 = Result<int>(0);
+        auto r2 = Result<int>();
+        auto r3 = Result<int>(result::Error("foo"));
+
+        CHECK_EQ(r1.valueOrThrow(), 0);
+        CHECK_THROWS_WITH_AS(r2.valueOrThrow(), "<result not initialized>", const result::NoResult&);
+        CHECK_THROWS_WITH_AS(r3.valueOrThrow(), "foo", const result::NoResult&);
+
+        r1.valueOrThrow() += 42;
+        CHECK_EQ(r1, Result(42));
+    }
+}
+
 TEST_CASE("to_string_for_print") {
     CHECK_EQ(to_string_for_print(Result<std::string>("abc")), "abc");
     CHECK_EQ(to_string_for_print(Result<std::string>()), "<error: <result not initialized>>");
