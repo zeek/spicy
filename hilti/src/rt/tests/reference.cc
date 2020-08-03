c9cdef61ae06d33691e0730e054b9f692b498e6d
Benjamin Bannier <benjamin.bannier@corelight.com>
2020-08-03 15:25:41 +0200

    Use `CHECK` macros for tests, `REQUIRE` macros for preconditions.

--- hilti/src/rt/tests/reference.cc
+++ hilti/src/rt/tests/reference.cc
@@ -16,8 +16,8 @@ struct T : public hilti::rt::trait::isStruct, hilti::rt::Controllable<T> {
     void foo(int y) {
         // Ensure we can reconstruct a value ref from "this".
         auto self = ValueReference<T>::self(this);
-        REQUIRE_EQ(x, y);
-        REQUIRE_EQ(self->x, y);
+        CHECK_EQ(x, y);
+        CHECK_EQ(self->x, y);
     }
 };
 
@@ -35,36 +35,36 @@ TEST_CASE("value-reference-int") {
     using T = int;
 
     ValueReference<T> x1;
-    REQUIRE_EQ(*x1, 0);
+    CHECK_EQ(*x1, 0);
 
     ValueReference<T> x2(42);
-    REQUIRE_EQ(*x2, 42);
+    CHECK_EQ(*x2, 42);
 
     ValueReference<T> x3(x2);
-    REQUIRE_EQ(*x3, 42);
+    CHECK_EQ(*x3, 42);
 
     x3 = 21;
-    REQUIRE_EQ(*x3, 21);
-    REQUIRE_EQ(*x2, 42);
+    CHECK_EQ(*x3, 21);
+    CHECK_EQ(*x2, 42);
 
     ValueReference<T> x4(std::move(x3));
-    REQUIRE_EQ(*x4, 21);
-    REQUIRE(x3.isNull());
+    CHECK(x3.isNull());
+    CHECK_EQ(*x4, 21);
 
     ValueReference<T> x5;
     x5 = std::move(x4);
-    REQUIRE_EQ(*x5, 21);
-    REQUIRE(x4.isNull());
+    CHECK(x4.isNull());
+    CHECK_EQ(*x5, 21);
 }
 
 TEST_CASE("value-reference-struct") {
     ValueReference<T> x1;
-    REQUIRE_EQ(x1->x, 0);
+    CHECK_EQ(x1->x, 0);
 
     T t;
     t.x = 42;
     ValueReference<T> x2(t);
-    REQUIRE_EQ(x2->x, 42);
+    CHECK_EQ(x2->x, 42);
 
     x2->x = 21;
     x2->foo(21);
@@ -79,11 +79,11 @@ TEST_CASE("value-reference-struct-self") {
     auto self = ValueReference<T>::self(&x1);
 
     self->x = 42;
-    REQUIRE_EQ(self->x, 42);
-    REQUIRE_EQ(x1.x, 42);
+    CHECK_EQ(self->x, 42);
+    CHECK_EQ(x1.x, 42);
 
-    REQUIRE_THROWS(StrongReference<T>{self});
-    REQUIRE_THROWS(WeakReference<T>{self});
+    CHECK_THROWS(StrongReference<T>{self});
+    CHECK_THROWS(WeakReference<T>{self});
 }
 
 
@@ -91,19 +91,19 @@ TEST_CASE("strong-reference") {
     using T = int;
 
     StrongReference<T> x0;
-    REQUIRE_FALSE(x0);
+    CHECK_FALSE(x0);
 
     StrongReference<T> x1(42);
     REQUIRE(x1);
-    REQUIRE_EQ(*x1, 42);
+    CHECK_EQ(*x1, 42);
 
     StrongReference<T> x2(x1);
     REQUIRE(x2);
-    REQUIRE_EQ(*x2, 42);
+    CHECK_EQ(*x2, 42);
 
     *x1 = 21;
-    REQUIRE_EQ(*x1, 21);
-    REQUIRE_EQ(*x2, 21);
+    CHECK_EQ(*x1, 21);
+    CHECK_EQ(*x2, 21);
 
     ValueReference<T> v1{1};
     ValueReference<T> v2{2};
@@ -112,10 +112,10 @@ TEST_CASE("strong-reference") {
     x2 = x1;
     v1 = v2;
 
-    REQUIRE_EQ(*x1, 2);
-    REQUIRE_EQ(*v1, 2);
-    REQUIRE_EQ(*x2, 2);
-    REQUIRE_EQ(*v2, 2);
+    CHECK_EQ(*x1, 2);
+    CHECK_EQ(*v1, 2);
+    CHECK_EQ(*x2, 2);
+    CHECK_EQ(*v2, 2);
 }
 
 struct Foo;
