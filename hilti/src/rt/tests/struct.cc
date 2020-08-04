6cf19ace3848962f5f33f4172e471dede93eb785
Benjamin Bannier <benjamin.bannier@corelight.com>
2020-08-04 11:12:11 +0200

    Cover `hilti/rt/types/struct.h` with unit tests.

--- hilti/src/rt/tests/struct.cc
+++ hilti/src/rt/tests/struct.cc
@@ -4,6 +4,8 @@
 
 #include <optional>
 
+#include <hilti/rt/extension-points.h>
+#include <hilti/rt/types/integer.h>
 #include <hilti/rt/types/struct.h>
 
 using namespace hilti::rt;
@@ -16,5 +18,20 @@ TEST_CASE("value_or_exception") {
     CHECK_THROWS_WITH_AS(struct_::value_or_exception(std::optional<int>(std::nullopt), "location:123"),
                          "struct attribute not set (location:123)", const AttributeNotSet&);
 }
+
+struct Test : trait::isStruct {
+    Test(int x) : _x(x), _y(x + 1) {}
+
+    template<typename F>
+    void __visit(F f) const {
+        f("_x", _x);
+        f("_y", _y);
+    }
+
+    int _x;
+    int _y;
+};
+
+TEST_CASE("to_string") { CHECK_EQ(to_string(Test(42)), "[$_x=42, $_y=43]"); }
 
 TEST_SUITE_END();
