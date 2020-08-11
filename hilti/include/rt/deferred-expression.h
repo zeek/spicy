b8de1173a539c9ec8120770a55a09abc8eb733c0
Benjamin Bannier <benjamin.bannier@corelight.com>
2020-08-11 15:06:35 +0200

    Fix `to_string_for_print` for `DeferredExpression`.
    Since `DeferredExpression` is a template itself we cannot specialize the
    master template of the `detail` namespace; instead we need to provide an
    overload.

--- hilti/include/rt/deferred-expression.h
+++ hilti/include/rt/deferred-expression.h
@@ -15,6 +15,9 @@ namespace hilti::rt {
  * Wrapper for an expression that's evaluation is deferred until requested.
  * The expression must be wrapped into a function call, and it's evaluation
  * is requested through the wrapper's call operator.
+ *
+ * The function should be stateless as it might be invoked an unspecified
+ * number of times.
  */
 template<typename Result>
 class DeferredExpression {
@@ -40,12 +43,14 @@ template<typename Result>
 inline std::string to_string(const DeferredExpression<Result>& x, adl::tag /*unused*/) {
     return hilti::rt::to_string(x());
 }
+} // namespace detail::adl
 
+// This function is declared as an overload since we cannot partially specialize
+// `hilti::detail::to_string_for_print` for `DeferredExpression<T>`.
 template<typename Result>
-inline std::string to_string_for_print(const DeferredExpression<Result>& x, adl::tag /*unused*/) {
+inline std::string to_string_for_print(const DeferredExpression<Result>& x) {
     return hilti::rt::to_string_for_print(x());
 }
-} // namespace detail::adl
 
 template<typename Result>
 inline std::ostream& operator<<(std::ostream& out, const DeferredExpression<Result>& x) {
