4f4a02491e4950e822370b98d4be379cdd5df222
Robin Sommer <robin@corelight.com>
2020-11-13 08:52:57 +0000

    Extend while-production to support termination of loop by look ahead.
    So far a while-production required an explicit condition to terminate
    the loop (which it turns out isn't actually used anywhere right now).
    This adds a 2nd version that doesn't need an expression and instead uses
    look-ahead parsing to decide when to terminate the loop: If the next
    input token indicates that we can match another instance of the loop's
    body, we go there; if the next token indicates that we can match
    whatever comes after the loop, we terminate; and if both or none of this
    is the case, that's an error. Internally, we create a little wrapper
    grammar to implement this, so that global look-ahead computation works
    as normal across such a while-loop.
    
    This logic is essentially all the same as what the grammar builder is
    currently doing already for parsing a vector through look-ahead---and
    we're going to replace that existing logic with this new loop in a
    subsequent commit.

--- spicy/toolchain/include/compiler/detail/codegen/productions/while.h
+++ spicy/toolchain/include/compiler/detail/codegen/productions/while.h
@@ -8,33 +8,70 @@
 
 #include <spicy/ast/types/unit.h>
 #include <spicy/compiler/detail/codegen/production.h>
+#include <spicy/compiler/detail/codegen/productions/look-ahead.h>
 
 namespace spicy::detail::codegen::production {
 
 /**
- * A production executing as long as a given boolean expression evaluates
- * to true.
+ * A production executing as long as either a given boolean expression evaluates
+ * to true, or, if no expression is provided, as determined by look-ahead symbols.
  */
 class While : public ProductionBase, public spicy::trait::isNonTerminal {
 public:
+    /**
+     * Constructor for a while-loop using an expression as the condition for termination.
+     */
     While(const std::string& symbol, Expression e, Production body, const Location& l = location::None)
-        : ProductionBase(symbol, l), _expression(std::move(e)), _body(std::move(body)) {}
+        : ProductionBase(symbol, l), _body(std::move(body)), _expression(std::move(e)) {}
 
-    const Expression& expression() const { return _expression; }
-    const Production& body() const { return _body; }
+    /**
+     * Constructor for a while-loop using look-ahead as the condition for
+     * termination. When using this constructor, `preprocessLookAhead()` must
+     * later be called with the grammar that the production has been inserted
+     * into.
+     */
+    While(const std::string& symbol, Production body, const Location& l =
+            location::None);
+
+    /** Returns the loop expression if passed into the corresponding constructor. */
+    const auto& expression() const { return _expression; }
+
+    /** Returns the body production as passed into any of the constructors.  */
+    const auto& body() const { return _body; }
+
+    /**
+     * Prepares the internal grammar representation for a look-ahead based
+     * loop. Must be called (only) when the corresponding constructor was used.
+     *
+     * @param grammar grammar that while-production is being part of.
+     */
+    void preprocessLookAhead(Grammar* grammar);
+
+    /**
+     * For a look-ahead loop, returns the internally generated `LookAhead`
+     * production that's being used for generating the code to terminate the
+     * loop. The production's 1st alternative corresponds to the case of
+     * terminating the loop, the 2nd alternative corresponds to executing the
+     * loop body. This method must be called only after `preprocessLookAhead()`.
+     */
+    const production::LookAhead& lookAheadProduction() const {
+        assert(_body_for_grammar); // set by preprocessLookAhead() return
+        return _body_for_grammar->as<production::LookAhead>();
+    }
 
     // Production API
-    std::vector<std::vector<Production>> rhss() const { return {{_body}}; }
+    std::vector<std::vector<Production>> rhss() const { return {{(_body_for_grammar ? *_body_for_grammar : _body)}}; }
     std::optional<spicy::Type> type() const { return {}; }
     bool nullable() const { return production::nullable(rhss()); }
     bool eodOk() const { return nullable(); }
     bool atomic() const { return false; }
     bool supportsSynchronize() const { return hasSize() || _body.supportsSynchronize(); }
-    std::string render() const { return hilti::util::fmt("while(%s): %s", _expression, _body.symbol()); }
+    std::string render() const;
 
 private:
-    Expression _expression;
     Production _body;
+    std::optional<Expression> _expression;
+    std::optional<Production> _body_for_grammar;
 };
 
 } // namespace spicy::detail::codegen::production
---
+++ spicy/toolchain/src/compiler/codegen/productions/while.cc
@@ -0,0 +1,43 @@
+// Copyright (c) 2020 by the Zeek Project. See LICENSE for details.
+
+#include <spicy/compiler/detail/codegen/grammar.h>
+#include <spicy/compiler/detail/codegen/production.h>
+#include <spicy/compiler/detail/codegen/production.h>
+#include <spicy/compiler/detail/codegen/productions/epsilon.h>
+#include <spicy/compiler/detail/codegen/productions/look-ahead.h>
+#include <spicy/compiler/detail/codegen/productions/resolved.h>
+#include <spicy/compiler/detail/codegen/productions/sequence.h>
+#include <spicy/compiler/detail/codegen/productions/while.h>
+
+using namespace spicy;
+using namespace spicy::detail;
+using namespace spicy::detail::codegen;
+
+production::While::While(const std::string& symbol, Production body, const Location& l)
+    : ProductionBase(symbol, l), _body(body) {}
+
+std::string production::While::render() const {
+    if ( _expression )
+        return hilti::util::fmt("while(%s): %s", *_expression, _body.symbol());
+    else
+        return hilti::util::fmt("while(<look-ahead-found>): %s", _body.symbol());
+}
+
+void production::While::preprocessLookAhead(Grammar* grammar) {
+    if ( _expression )
+        hilti::logger().internalError("preprocessLookAhead() must be called only for a look-ahead loop");
+
+    // We wrap the body into an additional little grammar that reflects the
+    // loop, so that computation of look-ahead symbols will work correctly.
+    // Specifically:
+    //
+    //      List1 -> Epsilon | List2
+    //      List2 -> Item List1
+    //
+    // This is Left-factored & right-recursive.
+    auto x = production::Unresolved();
+    auto l1 = production::LookAhead(symbol() + "_l1", production::Epsilon(location()), x, location());
+    auto l2 = production::Sequence(symbol() + "_l2", {_body, l1}, location());
+    grammar->resolve(&x, std::move(l2));
+    _body_for_grammar = std::move(l1);
+}
