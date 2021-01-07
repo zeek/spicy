babd22f09547baf75abe672e7ab1126b2e04103a
Benjamin Bannier <benjamin.bannier@corelight.com>
2021-01-07 15:18:03 +0100

    Mount procfs in FreeBSD CI so we can detect build directory.

--- ci/prepare_freebsd.sh
+++ ci/prepare_freebsd.sh
@@ -5,6 +5,10 @@ sysctl hw.model hw.machine hw.ncpu
 set -e
 set -x
 
+# Detection of whether we run in the build directory requires `/proc`.
+echo "proc /proc procfs rw,noauto 0 0" >> /etc/fstab
+mount /proc
+
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
 pkg install -y bash git cmake flex bison python3 ninja llvm11 zeek base64 ccache
 
