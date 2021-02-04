58bbe78936cebd4be58a7272321f8509b5df3bdb
Benjamin Bannier <benjamin.bannier@corelight.com>
2021-02-04 14:06:12 +0100

    Remove unused function typeOfExpressions.

--- hilti/toolchain/include/ast/builder/type.h
+++ hilti/toolchain/include/ast/builder/type.h
@@ -2,24 +2,12 @@
 
 #pragma once
 
-#include <utility>
-#include <vector>
-
-#include <hilti/ast/expression.h>
+#include <hilti/ast/meta.h>
 #include <hilti/ast/type.h>
 #include <hilti/ast/types/id.h>
 
 namespace hilti::builder {
 
-inline Type typeByID(::hilti::ID id, Meta m = Meta()) { return hilti::type::UnresolvedID(std::move(id), std::move(m)); }
-
-/**
- * Determines a common type for a list of expressions.
- *
- * @param e expressions
- * @return if *e* is non-empty and all expressions have the same type,
- * returns that type; otherwise returns ``type::Unknown``.
- */
-Type typeOfExpressions(const std::vector<Expression>& e);
+Type typeByID(::hilti::ID id, Meta m = Meta());
 
 } // namespace hilti::builder
--- hilti/toolchain/src/ast/builder/type.cc
+++ hilti/toolchain/src/ast/builder/type.cc
@@ -1,24 +1,9 @@
 // Copyright (c) 2020-2021 by the Zeek Project. See LICENSE for details.
 
-#include <vector>
+#include "hilti/ast/builder/type.h"
 
-#include <hilti/ast/builder/type.h>
-#include <hilti/ast/ctors/list.h>
-#include <hilti/ast/expression.h>
-#include <hilti/ast/types/unknown.h>
+#include <utility>
 
 using namespace hilti;
 
-Type builder::typeOfExpressions(const std::vector<Expression>& e) {
-    if ( e.empty() )
-        return type::unknown;
-
-    auto t = e.front().type();
-    for ( auto i = ++e.begin(); i != e.end(); i++ ) {
-        if ( i->type() != t )
-            // non-homogenous list
-            return type::unknown;
-    }
-
-    return t;
-}
+Type hilti::builder::typeByID(::hilti::ID id, Meta m) { return hilti::type::UnresolvedID(std::move(id), std::move(m)); }
