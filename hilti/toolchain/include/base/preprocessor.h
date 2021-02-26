a087912364b61ee76a83a67d2d33ba8a9c148a92
Robin Sommer <robin@corelight.com>
2021-02-26 12:28:46 +0000

    Factor preprocessor logic out of EVT parser.
    This makes the @if/@else/@endif logic reusable through a utility class
    coming with the HILTI toolchain library.
    
    Couple of extensions included as well:
    
        - Undefined IDs are now always treated as if they were set to zero.
    
        - We now support negation (`@if ! ...`) and `@if <id>` as a short
          cut for `@if <id> != 0`.
    
    It also fixes processing of nested if/else-chains, which was broken
    (turns out an existing test had recorded a wrong deadline here as well).
    
    Tests to come in a subsequent commit.

---
+++ hilti/toolchain/include/base/preprocessor.h
@@ -0,0 +1,67 @@
+// Copyright (c) 2020-2021 by the Zeek Project. See LICENSE for details.
+
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include <hilti/base/result.h>
+
+namespace hilti::util {
+/**
+ * Helper to preprocess @if/@else/@endif directives in lines of input.
+ *
+ * This currently supports the following set of directives:
+ *
+ *     - `@if [!] <id> ==|!=|<|<=|>|>= <integer>
+ *     - `@if [!] <id>
+ *     - `@else`
+ *     - `@endif`
+ **/
+class SourceCodePreprocessor {
+public:
+    using ID = std::string; //< type for identifiers
+    using Value = int;      //< type associated with integers (may expand this to a variant in the future)
+
+    /** Current inclusion state while processing input. */
+    enum class State {
+        Include, //< include line
+        Skip     //< skip line
+    };
+
+    /**
+     * Constructor.
+     *
+     * @param constants map of constants that preprocessor directives can work on
+     */
+    SourceCodePreprocessor(std::map<ID, Value> constants) : _constants(constants) {}
+
+    /**
+     * Process one preprocessor directive of the form `@<id> [expression]`.
+     *
+     * This evaluates the directive and updates internal state accordingly.
+     *
+     * @param directive a supported directive ID, which must start with `@`.
+     * @param expression string with expression that the directive is taking
+     * @result the new inclusion state for subsequent input, or an error if the directive could not be processed
+     */
+    Result<State> processLine(std::string_view directive, std::string_view expression = "");
+
+    /** Returns the current inclusion state. */
+    State state() const { return _stack.back() == 1 ? State::Include : State::Skip; }
+
+    /**
+     * Returns true if the proprocessor expects further directives that closed
+     * previously opened blocks
+     */
+    bool expectingDirective() { return _stack.size() > 1; }
+
+private:
+    Result<bool> _parseIf(const std::string_view& expression);
+
+    std::map<ID, Value> _constants;
+    std::vector<int> _stack = {1};
+};
+
+} // namespace hilti::util
