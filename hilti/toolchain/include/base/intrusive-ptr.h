149fde63c86a1ec042179113f2c45dbd7f5c30f4
Benjamin Bannier <benjamin.bannier@corelight.com>
2021-05-26 11:44:02 +0200

    Pull in previous definition with using decl instead of redeclaring.

--- hilti/toolchain/include/base/intrusive-ptr.h
+++ hilti/toolchain/include/base/intrusive-ptr.h
@@ -17,15 +17,7 @@ using NewRef = ::hilti::rt::intrusive_ptr::NewRef;
 using ManagedObject = ::hilti::rt::intrusive_ptr::ManagedObject;
 } // namespace intrusive_ptr
 
-template<class T, class... Ts>
-IntrusivePtr<T> make_intrusive(Ts&&... args) {
-    // Assumes that objects start with a reference count of 1!
-    return {intrusive_ptr::AdoptRef{}, new T(std::forward<Ts>(args)...)};
-}
-
-template<class T, class U>
-IntrusivePtr<T> cast_intrusive(IntrusivePtr<U> p) noexcept {
-    return {intrusive_ptr::AdoptRef{}, static_cast<T*>(p.release())};
-}
+using rt::cast_intrusive;
+using rt::make_intrusive;
 
 } // namespace hilti
