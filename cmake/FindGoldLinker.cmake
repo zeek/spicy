8a45c6415e4999c47bcb1997da8915366ee6f175
Benjamin Bannier <benjamin.bannier@corelight.com>
2021-12-07 18:52:23 +0100

    Reformat CMake files with cmake-format.

--- cmake/FindGoldLinker.cmake
+++ cmake/FindGoldLinker.cmake
@@ -1,17 +1,17 @@
-
 ## Enable Gold linker if available.
 
-OPTION(USE_GOLD "Use Gold linker" ON)
+option(USE_GOLD "Use Gold linker" ON)
 
 set(GOLD_FOUND "no")
 
-if ( USE_GOLD )
-    if ( UNIX AND NOT APPLE AND NOT LLD_PATH )
-        execute_process(COMMAND ${CMAKE_CXX_COMPILER} -fuse-ld=gold -Wl,--version ERROR_QUIET OUTPUT_VARIABLE ld_version)
+if (USE_GOLD)
+    if (UNIX AND NOT APPLE AND NOT LLD_PATH)
+        execute_process(COMMAND ${CMAKE_CXX_COMPILER} -fuse-ld=gold -Wl,--version ERROR_QUIET
+                        OUTPUT_VARIABLE ld_version)
         if ("${ld_version}" MATCHES "GNU gold")
             message(STATUS "Using Gold linker")
             set(GOLD_FOUND "yes")
-            set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=gold")
+            set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=gold")
             set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fuse-ld=gold")
             set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -fuse-ld=gold")
         else ()
