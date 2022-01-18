143a159fedb688c1b5f2286ee09e4837dd79d33f
Benjamin Bannier <benjamin.bannier@corelight.com>
2022-01-18 14:06:38 +0100

    fixup! fixup! Add confirm and reject methods.

--- spicy/toolchain/src/compiler/codegen/unit-builder.cc
+++ spicy/toolchain/src/compiler/codegen/unit-builder.cc
@@ -177,9 +177,11 @@ Type CodeGen::compileUnit(const type::Unit& unit, bool declare_only) {
     add_hook("0x25_print", {});
     add_hook("0x25_finally", {});
 
-    add_hook("0x25_confirmed", {}, {});
-    add_hook("0x25_rejected", {}, {});
-    add_hook("0x25_synced", {}, {});
+    // TODO(bbannier): Mark these hooks required by a new feature `synchronization`.
+    auto attr_sync = AttributeSet({Attribute("&always-emit")});
+    add_hook("0x25_confirmed", {}, attr_sync);
+    add_hook("0x25_rejected", {}, attr_sync);
+    add_hook("0x25_synced", {}, attr_sync);
 
     if ( unit.id() ) {
         ID typeID = ID(hilti::rt::replace(*unit.id(), ":", "_"));
