fb2e4ef9d2b0e1a961a4192383796d0593c9117d
Benjamin Bannier <benjamin.bannier@corelight.com>
2022-01-31 18:03:59 +0100

    Factor out helper to determine whether a type supports literal parsing.

---
+++ spicy/toolchain/src/ast/types.cc
@@ -0,0 +1,10 @@
+#include "spicy/ast/types.h"
+
+#include <hilti/ast/types/bytes.h>
+#include <hilti/ast/types/integer.h>
+#include <hilti/ast/types/regexp.h>
+
+bool spicy::type::supportsLiterals(const hilti::Type& t) {
+    return t.isA<hilti::type::Bytes>() || t.isA<hilti::type::RegExp>() || t.isA<hilti::type::SignedInteger>() ||
+           t.isA<hilti::type::UnsignedInteger>();
+}
