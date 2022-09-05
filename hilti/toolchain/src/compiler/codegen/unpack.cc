0ba767f91d1a7bab66aa2fe2a60ded99dae10f76
Benjamin Bannier <benjamin.bannier@corelight.com>
2022-09-05 14:29:36 +0200

    Make unpack Visitor void visitor.

--- hilti/toolchain/src/compiler/codegen/unpack.cc
+++ hilti/toolchain/src/compiler/codegen/unpack.cc
@@ -1,5 +1,7 @@
 // Copyright (c) 2020-2021 by the Zeek Project. See LICENSE for details.
 
+#include <optional>
+#include <string>
 #include <utility>
 
 #include <hilti/ast/detail/visitor.h>
@@ -16,27 +18,29 @@ using namespace hilti::detail;
 
 namespace {
 
-struct Visitor : hilti::visitor::PreOrder<std::string, Visitor> {
+struct Visitor : hilti::visitor::PreOrder<void, Visitor> {
     Visitor(CodeGen* cg, cxx::Expression data, const std::vector<cxx::Expression>& args)
         : cg(cg), data(std::move(data)), args(args) {}
     CodeGen* cg;
     cxx::Expression data;
     const std::vector<cxx::Expression>& args;
 
+    std::optional<std::string> _result;
+
     result_t operator()(const type::Address& n) {
-        return fmt("::hilti::rt::address::unpack(%s, %s, %s)", data, args[0], args[1]);
+        _result = fmt("::hilti::rt::address::unpack(%s, %s, %s)", data, args[0], args[1]);
     }
 
     result_t operator()(const type::UnsignedInteger& n) {
-        return fmt("::hilti::rt::integer::unpack<uint%d_t>(%s, %s)", n.width(), data, args[0]);
+        _result = fmt("::hilti::rt::integer::unpack<uint%d_t>(%s, %s)", n.width(), data, args[0]);
     }
 
     result_t operator()(const type::SignedInteger& n) {
-        return fmt("::hilti::rt::integer::unpack<int%d_t>(%s, %s)", n.width(), data, args[0]);
+        _result = fmt("::hilti::rt::integer::unpack<int%d_t>(%s, %s)", n.width(), data, args[0]);
     }
 
     result_t operator()(const type::Real& n) {
-        return fmt("::hilti::rt::real::unpack(%s, %s, %s)", data, args[0], args[1]);
+        _result = fmt("::hilti::rt::real::unpack(%s, %s, %s)", data, args[0], args[1]);
     }
 };
 
@@ -44,16 +48,18 @@ struct Visitor : hilti::visitor::PreOrder<std::string, Visitor> {
 
 cxx::Expression CodeGen::unpack(const hilti::Type& t, const Expression& data, const std::vector<Expression>& args) {
     auto cxx_args = util::transform(args, [&](const auto& e) { return compile(e, false); });
-    if ( auto x = Visitor(this, compile(data), cxx_args).dispatch(t) )
-        return cxx::Expression(*x);
+    auto v = Visitor(this, compile(data), cxx_args);
+    if ( v.dispatch(t); v._result )
+        return cxx::Expression(*v._result);
 
     logger().internalError("unpack failed to compile", t);
 }
 
 cxx::Expression CodeGen::unpack(const hilti::Type& t, const cxx::Expression& data,
                                 const std::vector<cxx::Expression>& args) {
-    if ( auto x = Visitor(this, data, args).dispatch(t) )
-        return cxx::Expression(*x);
+    auto v = Visitor(this, data, args);
+    if ( v.dispatch(t); v._result )
+        return cxx::Expression(*v._result);
 
     logger().internalError("unpack failed to compile", t);
 }
