ebe226922734e3a2a2db24a7e3c8a3d860cd6484
Benjamin Bannier <benjamin.bannier@corelight.com>
2022-09-05 13:28:54 +0200

    Fix _tryAs for Type nodes.

--- hilti/toolchain/include/base/type_erase.h
+++ hilti/toolchain/include/base/type_erase.h
@@ -17,6 +17,11 @@
 #include <hilti/base/optional-ref.h>
 #include <hilti/base/util.h>
 
+namespace hilti {
+class Type;
+class TypeBase;
+} // namespace hilti
+
 namespace hilti::util::type_erasure {
 
 // If this defined, we track the number of type-erased instances by their
@@ -240,6 +245,15 @@ public:
 private:
     template<typename T>
     const T* _tryAs() const {
+        // TypeBase maintains its own hierarchy.
+        if constexpr ( std::is_base_of_v<TypeBase, T> )
+            if ( auto&& t = _tryAs<Type>() ) {
+                if ( t->template isA<T>() )
+                    return &t->template as<T>();
+                else
+                    return nullptr;
+            }
+
         if constexpr ( std::is_base_of<ErasedBase, T>::value )
             return static_cast<const T*>(this);
 
@@ -260,6 +274,15 @@ private:
 
     template<typename T>
     T* _tryAs() {
+        // TypeBase maintains its own hierarchy.
+        if constexpr ( std::is_base_of_v<TypeBase, T> )
+            if ( auto&& t = _tryAs<Type>() ) {
+                if ( t->template isA<T>() )
+                    return &t->template as<T>();
+                else
+                    return nullptr;
+            }
+
         if constexpr ( std::is_base_of<ErasedBase, T>::value )
             return static_cast<T*>(this);
 
