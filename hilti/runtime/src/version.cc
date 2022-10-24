27e946d8ce657095a25f531eb16fe133cfa6f09d
Robin Sommer <robin@corelight.com>
2022-10-24 10:22:05 +0200

    Embed a C function into hilti-rt that encodes our version in its name.
    If a host applications links against this, it will fail to load if the
    runtime version changes without a rebuild. This is the same approach Zeek
    is using to ensure plugins are loaded only into the Zeek version they
    were compiled for.
    
    Addresses #1221.
    Addresses https://github.com/zeek/spicy-plugin/issues/82

---
+++ hilti/runtime/src/version.cc
@@ -0,0 +1,5 @@
+// Copyright (c) 2020-2021 by the Zeek Project. See LICENSE for details.
+
+#include <hilti/rt/autogen/version.h>
+
+const char* PROJECT_VERSION_FUNCTION() { return PROJECT_VERSION_STRING_LONG; }
