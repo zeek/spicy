d32c68b903b48254568a59631b9102adfb33e20a
Robin Sommer <robin@corelight.com>
2023-01-19 12:47:58 +0100

    Add a global configuration object to the Spicy runtime.
    We didn't have a mechanism yet for a host application to configure the
    runtime library's behavior. This follows what we do on the HILTI-side
    to provide that capability. Right now, there are no actual options
    available yet, but that will come.

---
+++ spicy/runtime/src/configuration.cc
@@ -0,0 +1,23 @@
+// Copyright (c) 2020-2021 by the Zeek Project. See LICENSE for details.
+
+#include <hilti/rt/logging.h>
+#include <spicy/rt/configuration.h>
+#include <spicy/rt/global-state.h>
+#include <spicy/rt/init.h>
+
+using namespace spicy::rt;
+using namespace spicy::rt::detail;
+
+const Configuration& configuration::get() {
+    if ( ! globalState()->configuration )
+        globalState()->configuration = std::make_unique<spicy::rt::Configuration>();
+
+    return *globalState()->configuration;
+}
+
+void configuration::set(Configuration cfg) {
+    if ( isInitialized() )
+        hilti::rt::fatalError("attempt to change configuration after library has already been initialized");
+
+    *globalState()->configuration = std::move(cfg);
+}
