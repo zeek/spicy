29f1f2deaf6745c6895280ccf240117b541043f9
Robin Sommer <robin@corelight.com>
2023-01-19 12:47:58 +0100

    Add a global configuration object to the Spicy runtime.
    We didn't have a mechanism yet for a host application to configure the
    runtime library's behavior. This follows what we do on the HILTI-side
    to provide that capability. Right now, there are no actual options
    available yet, but that will come.

---
+++ spicy/runtime/include/configuration.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2020-2021 by the Zeek Project. See LICENSE for details.
+
+#pragma once
+
+namespace spicy::rt {
+
+/** Configuration parameters for the Spicy runtime system. */
+struct Configuration {
+    Configuration() {}
+};
+
+namespace configuration {
+
+/**
+ * Returns the current global configuration. To change the
+ * configuration, modify it and then pass it back to `set()`.
+ */
+extern const Configuration& get();
+
+/**
+ * Sets new configuration values. Usually one first retrieves the current
+ * configuration with `get()` to then apply any desired changes to it.
+ *
+ * @param cfg complete set of new configuration values
+ */
+extern void set(Configuration cfg);
+
+} // namespace configuration
+} // namespace spicy::rt
