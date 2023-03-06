e8112b932a3ede7a0e77a4ad1dfef494bf161063
Benjamin Bannier <benjamin.bannier@corelight.com>
2023-03-06 22:15:28 +0100

    Initialize `Bytes` internal control block for all constructors.
    `Bytes` inherits constructors from its base class. We previously would
    not initialize the internal control block for these constructors so some
    operations involving iterators on such `Bytes` instances would have
    failed, even thought the controlled object was still valid.
    
    With this patch we always set up the control block, no matter which
    constructor is used.
    
    Closes #1390.
    
    (cherry picked from commit 71cff50262bd26c3e36cd708c5063e87a77eacbf)

--- hilti/runtime/include/types/bytes.h
+++ hilti/runtime/include/types/bytes.h
@@ -173,9 +173,9 @@ public:
      */
     Bytes(std::string s, bytes::Charset cs);
 
-    Bytes(Base&& str) : Base(std::move(str)), _control(std::make_shared<Base*>(static_cast<Base*>(this))) {}
-    Bytes(const Bytes& xs) : Base(xs), _control(std::make_shared<Base*>(static_cast<Base*>(this))) {}
-    Bytes(Bytes&& xs) noexcept : Base(std::move(xs)), _control(std::make_shared<Base*>(static_cast<Base*>(this))) {}
+    Bytes(Base&& str) : Base(std::move(str)) {}
+    Bytes(const Bytes& xs) : Base(xs) {}
+    Bytes(Bytes&& xs) noexcept : Base(std::move(xs)) {}
 
     /** Replaces the contents of this `Bytes` with another `Bytes`.
      *
@@ -511,7 +511,7 @@ public:
 
 private:
     friend bytes::Iterator;
-    std::shared_ptr<Base*> _control;
+    std::shared_ptr<Base*> _control = std::make_shared<Base*>(static_cast<Base*>(this));
 
     void invalidateIterators() { _control = std::make_shared<Base*>(static_cast<Base*>(this)); }
 };
