aeb1bccc89c464e5742898284219ab598ec535e9
Benjamin Bannier <benjamin.bannier@corelight.com>
2023-09-28 15:53:51 +0200

    Add feature guards to accesses of a unit's `__position`.
    Access of `__position` triggers a random access functionality. In order
    to distinguish our internal uses from accesses due to user code, most
    access in our generated code should be guarded with a feature constant
    (`if` or ternary).
    
    In this patch add proper guards for a couple instances where we did not
    do that correctly. That mishap caused all units with containers to be
    random access (even the root unit) which in turn could have lead to
    e.g., unbounded memory growth, or runtime overhead due to generation and
    execution of unneeded code, or expensive cleanup on very large untrimmed
    inputs.
    
    Closes #1549.
    
    (cherry picked from commit acd3844f0c94e2c6025f89453e6a0519f9f2025e)

--- spicy/toolchain/src/compiler/codegen/parser-builder.cc
+++ spicy/toolchain/src/compiler/codegen/parser-builder.cc
@@ -641,8 +641,13 @@ struct ProductionVisitor
         // can restore it later.
         std::optional<Expression> pre_container_offset;
         if ( field && field->isContainer() ) {
+            const auto id = hilti::util::replace(state().unit_id, ":", "_");
             pre_container_offset =
-                builder()->addTmp("pre_container_offset", builder::member(state().self, "__position"));
+                builder()->addTmp("pre_container_offset",
+                                  builder::ternary(builder::id(
+                                                       ID(hilti::rt::fmt("__feat%%%s%%%s", id, "uses_random_access"))),
+                                                   builder::member(state().self, "__position"),
+                                                   builder::optional(hilti::type::stream::Iterator())));
         }
 
         if ( field && field->convertExpression() ) {
@@ -736,10 +741,16 @@ struct ProductionVisitor
         // elements inside e.g., this unit's fields hooks. Temporarily restore the previously stored offset.
         std::optional<Expression> prev;
         if ( pre_container_offset ) {
-            prev = builder()->addTmp("prev", builder::member(state().self, "__position"));
-            builder()->addAssign(builder::member(state().self, "__position"), *pre_container_offset);
-        }
+            const auto id = hilti::util::replace(state().unit_id, ":", "_");
+            prev = builder()->addTmp("prev", builder::ternary(builder::id(ID(hilti::rt::fmt("__feat%%%s%%%s", id,
+                                                                                            "uses_random_access"))),
+                                                              builder::member(state().self, "__position"),
+                                                              builder::optional(hilti::type::stream::Iterator())));
 
+            pb->guardFeatureCode(state().unit, {"uses_random_access"}, [&]() {
+                builder()->addAssign(builder::member(state().self, "__position"), *pre_container_offset);
+            });
+        }
 
         HILTI_DEBUG(spicy::logging::debug::ParserBuilder, fmt("- post-parse field: %s", field->id()));
 
@@ -819,7 +830,8 @@ struct ProductionVisitor
             popState();
 
         if ( prev )
-            builder()->addAssign(builder::member(state().self, "__position"), *prev);
+            pb->guardFeatureCode(state().unit, {"uses_random_access"},
+                                 [&]() { builder()->addAssign(builder::member(state().self, "__position"), *prev); });
 
         if ( field->condition() )
             popBuilder();
