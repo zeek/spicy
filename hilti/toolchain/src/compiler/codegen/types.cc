ab5c1063ba3eb817f5a6c339ab59e117a234c019
Robin Sommer <robin@corelight.com>
2025-01-31 11:28:53 +0100

    Add a `to_string()` callback to our custom runtime type information.
    Now a host application can render a value into a string that it has
    retrieved through the runtime introspection API. The
    `type_info::Value` class gains a `to_string()` method as the
    user-facing entry point to that.

--- hilti/toolchain/src/compiler/codegen/types.cc
+++ hilti/toolchain/src/compiler/codegen/types.cc
@@ -1138,8 +1138,19 @@ const CxxTypeInfo& CodeGen::_getOrCreateTypeInfo(QualifiedType* t) {
                 logger().internalError(fmt("codegen: type %s does not have a dynamic type info visitor", *t), t);
 
             const auto& id_init = (t->type()->typeID() ? fmt("\"%s\"", t->type()->typeID()) : std::string("{}"));
-            auto init = fmt("{ %s, \"%s\", new %s }", id_init,
-                            util::escapeUTF8(display.str(), util::render_style::UTF8::EscapeQuotes), *x);
+
+            std::string to_string;
+
+            if ( auto x = t->type()->tryAs<type::Library>() )
+                // Library types cannot be rendered into strings, just hardcode the type's name.
+                to_string = fmt("[](const void *self) { return \"<%s>\"s; }", x->cxxName());
+            else
+                to_string =
+                    fmt("[](const void *self) { return hilti::rt::to_string(*reinterpret_cast<const %s*>(self)); }",
+                        compile(t, codegen::TypeUsage::Storage));
+
+            auto init = fmt("{ %s, \"%s\", %s, new %s }", id_init,
+                            util::escapeUTF8(display.str(), util::render_style::UTF8::EscapeQuotes), to_string, *x);
 
             ti.declaration = cxx::declaration::Constant(tid, "::hilti::rt::TypeInfo", init, "");
 
