9a168d0378d8d5257ce7e2f37693a5d4688a5391
Benjamin Bannier <benjamin.bannier@corelight.com>
2025-06-25 12:41:08 +0200

    Switch from `linb::any` to `std::any`.
    We introduced `linb::any` to work around the broken implementation of
    `std::any` with the version of GCC shipping with debian-10. We have in
    the meantime dropped support for debian-10, and also require a compiler
    with support for C++20, so `std::any` should be well-supported.

--- hilti/runtime/include/any.h
+++ hilti/runtime/include/any.h
@@ -2,22 +2,12 @@
 
 #pragma once
 
-#include <hilti/rt/3rdparty/any/any.hpp>
+#include <any> // IWYU pragma: keep
 
 namespace hilti::rt {
 
-// We ran into some trouble with older versions of std::any on some
-// platforms, so we're bringing in an external implementation for the time
-// being. Specifically, we observed the issue in
-// https://stackoverflow.com/a/52414724 on Debian 10, with no obvious way to
-// work around it. See https://github.com/zeek/spicy/issues/629 for the full
-// discussion.
-//
-// Note that this implementation pulls in the *experimental* any interface,
-// which is slightly different from the standardized C++17 API.
-
-using linb::any;          // NOLINT(misc-unused-using-decls)
-using linb::any_cast;     // NOLINT(misc-unused-using-decls)
-using linb::bad_any_cast; // NOLINT(misc-unused-using-decls)
+using std::any;          // NOLINT(misc-unused-using-decls)
+using std::any_cast;     // NOLINT(misc-unused-using-decls)
+using std::bad_any_cast; // NOLINT(misc-unused-using-decls)
 
 } // namespace hilti::rt
