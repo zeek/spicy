e0aff9421499050a170a55f856c335286dc8cdb1
Benjamin Bannier <benjamin.bannier@corelight.com>
2025-10-10 08:27:54 +0200

    Prefer `cmake-core` over `cmake` in FreeBSD CI.
    It looks like `cmake` has spotty availability across versions, just use
    `cmake-core` instead which should already have what we need.

--- ci/prepare_freebsd.sh
+++ ci/prepare_freebsd.sh
@@ -12,7 +12,7 @@ echo "proc /proc procfs rw,noauto 0 0" >> /etc/fstab
 mount /proc
 
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
-pkg install -y bash git cmake flex bison python3 ninja base64 ccache
+pkg install -y bash git cmake-core flex bison python3 ninja base64 ccache
 
 pyver=$(python3 -c 'import sys; print(f"py{sys.version_info[0]}{sys.version_info[1]}")')
 pkg install -y "$pyver"-pip
