89b0e483252193a5de2b48b7cb3b193defc666fa
Robin Sommer <robin@corelight.com>
2025-11-03 13:43:40 +0100

    Add API to HILTI's scope builder to report if changes to existing scopes were made.

--- hilti/toolchain/include/compiler/detail/scope-builder.h
+++ hilti/toolchain/include/compiler/detail/scope-builder.h
@@ -10,4 +10,16 @@ namespace hilti::detail::scope_builder {
 /** Implements the corresponding functionality for the default HILTI compiler plugin. */
 void build(Builder* builder, ASTRoot* root);
 
+/**
+ * Builds up the scope like build(), but also reports whether any changes have
+ * been made during the process compared to how the scopes looked like before.
+ *
+ * Note this doesn't report if there are pre-existing IDs that weren't accessed
+ * at all during the current run. It will return "unchanged" in that case if no
+ * changes were made beyond such IDs.
+ *
+ * @return true if any scopes were modified, false otherwise.
+ */
+bool buildToValidate(Builder* builder, ASTRoot* root);
+
 } // namespace hilti::detail::scope_builder
