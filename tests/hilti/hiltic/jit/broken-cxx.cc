e95e07a1ee96a4327b172fc2bec5906d1acf022c
Benjamin Bannier <benjamin.bannier@corelight.com>
2026-04-01 15:55:53 +0200

    fixup! [windows] make spicy btest pass on windows

--- tests/hilti/hiltic/jit/broken-cxx.cc
+++ tests/hilti/hiltic/jit/broken-cxx.cc
@@ -1,6 +1,6 @@
 // @TEST-EXEC-FAIL: HILTI_JIT_SHOW_CXX_OUTPUT=1 hiltic -j %INPUT 2>output
-// @TEST-EXEC: grep -qi "Broken\|error" hilti-jit-error.log
-// @TEST-EXEC: grep -q "compilation failed\|Broken" output
+// @TEST-EXEC: grep -qi "error" hilti-jit-error.log
+// @TEST-EXEC: grep -q "compilation failed" output
 //
 // Check that compiling broken C++ code doesn't produce any leaks or other
 // unexpected output. Because the actual output depends on the underlying
