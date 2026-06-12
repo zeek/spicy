a5a8fb1a3eb9890918526bd1e3f877f5100d1d25
Arne Welzel <arne.welzel@corelight.com>
2026-06-12 13:21:39 +0200

    safe-math: Also ignore -Wpedantic for SafeInt include

--- hilti/runtime/src/safe-math.cc
+++ hilti/runtime/src/safe-math.cc
@@ -7,9 +7,18 @@ using namespace hilti::rt;
 
 [[noreturn]] inline static void safe_math_fail(const char* /*msg*/) { throw OutOfRange("integer value out of range"); }
 
+#if defined(__clang__) || defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+
 #define SAFE_MATH_FAIL_DEFINED
 extern "C" {
 #include <hilti/rt/3rdparty/SafeInt/safe_math_impl.h>
 }
+
+#if defined(__clang__) || defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 int64_t integer::safe_negate(uint64_t x) { return safe_sub_int64_uint64(0, x); }
